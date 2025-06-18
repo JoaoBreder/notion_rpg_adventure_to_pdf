cae9fdbfea36d7b555870be15353ac09601cad61
João Breder <joao@directon.com.br>
2025-06-18 00:41:25 -0300

    init(tailwindcss): Adicionado tailwindcss ao projeto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,scss,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                'white-color': {
+                    25: '#FFFFFF',
+                    50: '#FFFFFF',
+                    100: '#FFFFFF',
+                    200: '#FFFFFF',
+                    300: '#FFFFFF',
+                    400: '#FFFFFF',
+                    500: '#FFFFFF',
+                    600: '#FFFFFF',
+                    700: '#FFFFFF',
+                    800: '#FFFFFF',
+                    900: '#FFFFFF',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
